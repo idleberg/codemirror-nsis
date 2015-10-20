163f023244e632c5a5d3f8fd60ceeabcca4bcc9c
Jan T. Sott <jan@idleberg.com>
2015-10-20 18:49:56 +0200

    remove test

--- nsis.js
+++ nsis.js
@@ -73,7 +73,7 @@ CodeMirror.defineSimpleMode("nsis",{
     {regex: /\$[\w]+/, token: "variable"},
 
     // Constant
-    {regex: /\${[\w]+}/,token: "variable-3"},
+    {regex: /\${[\w]+}/,token: "variable-2"},
 
     // Language String
     {regex: /\$\([\w]+\)/,token: "variable-3"}
